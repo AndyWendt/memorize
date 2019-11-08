b6df84205438efbd2fb64a65bd2593c3efc9da09
Andy Wendt <awendt@mavenlink.com>
2019-11-07 18:33:13 -0700

    Refactor... refactor... refactor...

--- lib/memorize/commands/file.rb
+++ lib/memorize/commands/file.rb
@@ -23,37 +23,56 @@ module Memorize
       attr_reader :prompt, :output
 
       def question_run(questions)
-        return unless questions.length > 0
+        return if questions.empty?
 
         do_over = questions.shuffle.reduce([]) do |redo_questions, question|
-          prompt.say("\n")
-          answer = prompt.multiline(question['question']).join("\n")
-          prompt.say("\n")
+          answer = ask_the_question(question)
+          display_the_question(question)
+          display_the_answer(question)
+          display_their_answer(answer)
+          ask_about_improvement
 
-          prompt.say('=====Question=====')
-          prompt.say(question['question'])
-          prompt.say("\n")
-
-          prompt.say('=====Answer=====')
-          output.puts parsed = TTY::Markdown.parse(question['answer'])
-          prompt.say("\n")
-
-          prompt.say('=====Your Answer=====')
-          output.puts TTY::Markdown.parse(answer)
-          prompt.say("\n")
-
-          improvement = prompt.ask('What could be improved about this answer?')
-          prompt.say('=====Improvement=====')
-          output.puts improvement
-          prompt.say("\n")
-
-          ask_question_again = prompt.yes?('Ask question again?')
-
-          redo_questions.push(question) if ask_question_again
+          redo_questions.push(question) if ask_again?
           redo_questions
         end
 
         question_run(do_over)
+      end
+
+      def ask_again?
+        prompt.yes?('Ask question again?')
+      end
+
+      def ask_the_question(question)
+        prompt.say("\n")
+        answer = prompt.multiline(question['question']).join("\n")
+        prompt.say("\n")
+        answer
+      end
+
+      def display_the_question(question)
+        prompt.say('=====Question=====')
+        prompt.say(question['question'])
+        prompt.say("\n")
+      end
+
+      def display_the_answer(question)
+        prompt.say('=====Answer=====')
+        output.puts parsed = TTY::Markdown.parse(question['answer'])
+        prompt.say("\n")
+      end
+
+      def display_their_answer(answer)
+        prompt.say('=====Your Answer=====')
+        output.puts TTY::Markdown.parse(answer)
+        prompt.say("\n")
+      end
+
+      def ask_about_improvement
+        improvement = prompt.ask('What could be improved about this answer?')
+        prompt.say('=====Improvement=====')
+        output.puts improvement
+        prompt.say("\n")
       end
 
       def questions
