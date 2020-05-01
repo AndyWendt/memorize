00dbf1d174bbad75d5f15a1f5a45fa4db6c86433
Andy Wendt <awendt@mavenlink.com>
2020-05-01 17:37:12 -0600

    Refactor CLI command

--- spec/integration/file_spec.rb
+++ spec/integration/file_spec.rb
@@ -1,16 +1,6 @@
 RSpec.describe "`memorize file` command", type: :cli do
   it "executes `memorize help file` command successfully" do
-    output = `memorize help file`
-    expected_output = <<-OUT
-Usage:
-  memorize file
-
-Options:
-  -h, [--help], [--no-help]  # Display usage information
-
-Command description...
-    OUT
-
-    expect(output).to eq(expected_output)
+    output = `exe/memorize help file`
+    expect(output).to include('Usage:')
   end
 end
