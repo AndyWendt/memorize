bee79ff9ceaa64171abfbda4fb46cb55114cee31
Andy Wendt <awendt@mavenlink.com>
2020-05-01 17:32:19 -0600

    Fix file spec

--- spec/unit/file_spec.rb
+++ spec/unit/file_spec.rb
@@ -1,13 +1,13 @@
-require 'lib/memorize/commands/file'
+require_relative '../../lib/memorize/commands/file'
 
 RSpec.describe Memorize::Commands::File do
   it "executes `file` command successfully" do
     output = StringIO.new
     options = {}
-    command = Memorize::Commands::File.new(options)
+    command = Memorize::Commands::File.new(options, 'spec/support/empty_yaml_files/empty.yml')
 
     command.execute(output: output)
 
-    expect(output.string).to eq("OK\n")
+    expect(output.string).to eq("")
   end
 end
